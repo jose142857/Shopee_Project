7ad994b4a68e4bdcf1246d2a80d9f88b701f00d4
jose1428 <nguyenvanthi168@gmail.com>
2023-02-09 23:13:29 +0900

    created search history

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,32 @@
-// if click outside then close  modal
+// CLOSE MODAL LOGIN & RIGISTER
 
 var modal = document.getElementById("id-modal");
-
 var modalLogin = document.getElementById("id-modal-login");
+var modalSearch = document.getElementById('id__modal__search');
+
 window.onclick = function (event) {
-  if (event.target == modalLogin) {
-    modalLogin.style.display = "none";
-  }
+
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  if (event.target == modalLogin) {
+    modalLogin.style.display = "none";
+  }
+  if (event.target != modalSearch) {
+    modalSearch.style.display = "none";
+    console.log(123)
+  }
+  console.log(event.target)
+  console.log(modalSearch)
+
+
+
+
 };
 
-// function closeModal() {
-//     document.querySelector(".modal").style.display = "none"
-// }
-// document.addEventListener(
-//     "click",
-//     function (event) {
-//         // If user clicks outside the modal window, then close modal by calling closeModal()
-//         if (event.target.closest('.modal__body')) {
-//             closeModal()
-//         }
-//     },
-//     false
-// )
+// OPEN MODAL SEARCH
+
+function fnc_openModalSearch() {
+  document.getElementById('id__modal__search').style.display = 'block';
+}
+
