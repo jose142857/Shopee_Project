d418f35ce9ea335bca75c949b53cfd5149ee364e
Van Thi <nguyenvanthi168@gmail.com>
2023-02-09 11:53:29 +0900

    finished login form and modal animation

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,27 @@
-// var modal = document.getElementById('id-modal');
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
+// if click outside then close  modal
 
-//     }
+var modal = document.getElementById("id-modal");
+
+var modalLogin = document.getElementById("id-modal-login");
+window.onclick = function (event) {
+  if (event.target == modalLogin) {
+    modalLogin.style.display = "none";
+  }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// function closeModal() {
+//     document.querySelector(".modal").style.display = "none"
 // }
-// console.log(modal);
-
-
-function closeModal() {
-    document.querySelector(".modal").style.display = "none"
-}
-document.addEventListener(
-    "click",
-    function (event) {
-        // If user clicks outside the modal window, then close modal by calling closeModal()
-        if (event.target.closest(".modal__body")) {
-            closeModal()
-        }
-    },
-    false
-)
-
+// document.addEventListener(
+//     "click",
+//     function (event) {
+//         // If user clicks outside the modal window, then close modal by calling closeModal()
+//         if (event.target.closest('.modal__body')) {
+//             closeModal()
+//         }
+//     },
+//     false
+// )
